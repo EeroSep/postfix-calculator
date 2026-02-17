eed956c469a4bead39635ebb413461722966d7ff
Eero Seppanen <ejs.seppanen@gmail.com>
2026-02-17 20:12:16 +0200

    sum ja exchange

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,14 @@ char get_command()
       command = tolower(command);
       if (command == '?' || command == '=' || command == '+' ||
           command == '-' || command == '*' || command == '/' ||
-          command == 'q' ) waiting = false;
+          command == 'q' || command == 'x' || command == 's') waiting = false;
 
 
       else {
          std::cout << "Please enter a valid command:"   << std::endl
               << "[?]push to stack   [=]print top" << std::endl
               << "[+] [-] [*] [/]   are arithmetic operations" << std::endl
+              << "[x]Exchange [s]sum" << std::endl
               << "[Q]uit." << std::endl;
       }
    }
@@ -29,6 +30,7 @@ char get_command()
 bool do_command(char command, Extended_stack &stack)
 {
    double p, q;
+   double sum = 0;
    switch (command){
    case '?':
     std::cout << "Enter a real number: " << std::flush;
@@ -64,16 +66,107 @@ bool do_command(char command, Extended_stack &stack)
         }
     }
     break;
+    case '-':
+    if (stack.top(p) == underflow) {
+        std::cout << "Stack empty." << std::endl;
+    }
+    else {        
+        stack.pop();
+        if (stack.top(q) == underflow) {
+            std::cout << "Stack has only one number." << std::endl;
+            stack.push(p);
+        }
+        else {
+            stack.pop();
+            if (stack.push(q - p) == overflow) {
+                std::cout << "Stack overflow, result lost." << std::endl;
+            }
+        }
+    }
+    break;
+    case '*':
+    if (stack.top(p) == underflow) {
+        std::cout << "Stack empty." << std::endl;
+    }
+    else {
+        stack.pop();
+        if (stack.top(q) == underflow) {
+            std::cout << "Stack has only one number." << std::endl;
+            stack.push(p);
+        }
+        else {
+            stack.pop();
+            if (stack.push(p * q) == overflow) {
+                std::cout << "Stack overflow, result lost." << std::endl;
+            }
+        }
+    }
+    break;
+    case '/':
+    if (stack.top(p) == underflow) {
+        std::cout << "Stack empty." << std::endl;
+    }
+    else {
+        stack.pop();
+        if (stack.top(q) == underflow) {
+            std::cout << "Stack has only one number." << std::endl;
+            stack.push(p);
+        }
+        else {
+            stack.pop();
+            if (q == 0) {
+                std::cout << "Division by zero." << std::endl;
+            }
+            else if (stack.push(q / p) == overflow) {
+                std::cout << "Stack overflow, result lost." << std::endl;
+            }
+        }
+    }
+    break;
     case 'q':
-        std::cout << "Calculation finsihed." << std::endl;
+        std::cout << "Calculation finished." << std::endl;
         return false;
     break;
+    case 'x':
+    if (stack.top(p) == underflow) {
+        std::cout << "Stack empty." << std::endl;
+    }
+    else {
+        Extended_stack temp;
+        stack.pop();
+        if (stack.top(q) == underflow) {
+            std::cout << "Stack has only one number." << std::endl;
+            stack.push(p);
+        }
+        else {
+            stack.pop();
+            stack.push(p);
+            stack.push(q);
+        }
+    }
+    break;
+    case 's':
+    
+    if (stack.top(p) == underflow) {
+        std::cout << "Stack empty." << std::endl;
+        break;
+    }
+    while (stack.top(p) != underflow) {
+        sum += p;
+        stack.pop();
+    }
+    if (stack.push(sum) == overflow) {
+        std::cout << "Stack overflow, sum lost." << std::endl;
+    }
+    break;
+
     default:
         std::cout << "Command not implemented." << std::endl;
     break;
    }
     return true;
 }
+
 int main() {
     
     Extended_stack stored_numbers;
--- src/utility.h
+++ src/utility.h
@@ -7,6 +7,8 @@ void instructions() {
     std::cout << "Type '?' to push a number onto the stack." << std::endl;
     std::cout << "Type '=' to print the top number of the stack." << std::endl;
     std::cout << "Type '+', '-', '*', '/' to perform operations." << std::endl;
+    std::cout << "Type 'x' to exchange the top two numbers on the stack." << std::endl;
+    std::cout << "Type 's' to sum all numbers on the stack." << std::endl;
     std::cout << "Type 'q' to quit the program." << std::endl;
 }
     
